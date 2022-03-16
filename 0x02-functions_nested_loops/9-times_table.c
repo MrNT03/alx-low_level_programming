1f5146cb12cd10067c06c70f8b4ec6c835e02470
Natnael <Natnaeltes@gmail.com>
2022-03-16 12:18:25 -0700

    na

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 1; mult <= 9; mult66)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -19,9 +19,9 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
-				_putchaar(' ');
+				_putchar(' ');
 			else
-				_putchar((prod / 10) = '0');
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
